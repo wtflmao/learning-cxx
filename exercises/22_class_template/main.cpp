ddc581f25a1b0a7c4b7df5ce66ae5ef5a0533fd2
wtflmao <19224718+wtflmao@users.noreply.github.com>
2025-01-19 11:52:25 +0800

    commit

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include "../exercise.h"
+#include <cstring>
 
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
@@ -10,6 +11,9 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        std::memcpy(shape, shape_, 4 * sizeof(unsigned int));
+        size = shape[0] * shape[1] * shape[2] * shape[3];
+
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,38 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        int oi, oj, ok, ol;
+        for(unsigned i = 0; i < shape[0]; i++) {
+            if (others.shape[0] != shape[0]) {
+                oi = 0;
+            } else {
+                oi = i;
+            }
+            for(unsigned j = 0; j < shape[1]; j++) {
+                if (others.shape[1] != shape[1]) {
+                    oj = 0;
+                } else {
+                    oj = j;
+                }
+                for(unsigned k = 0; k < shape[2]; k++) {
+                    if (others.shape[2] != shape[2]) {
+                        ok = 0;
+                    } else {
+                        ok = k;
+                    }
+                    for(unsigned l = 0; l < shape[3]; l++) {
+                        if (others.shape[3] != shape[3]) {
+                            ol = 0;
+                        } else {
+                            ol = l;
+                        }
+                        auto index = i * shape[1] * shape[2] * shape[3] + j * shape[2] * shape[3] + k * shape[3] + l;
+                        auto oindex = oi * others.shape[1] * others.shape[2] * others.shape[3] + oj * others.shape[2] * others.shape[3] + ok * others.shape[3] + ol;
+                        data[index] += others.data[oindex];
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
